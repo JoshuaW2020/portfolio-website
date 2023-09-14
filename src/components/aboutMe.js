ea078d453edf6d2cb6a22e78a8bab9433669c4cd
JoshuaW2020 <joshua.walsh1998@gmail.com>
2023-09-14 16:42:30 -0700

    Navbar Features and Basic Home Section
    Adds more navbar features, such as hovering and responsiveness. Also
    adds basic layout/responsive support for home page, will need final
    touching up.

--- src/components/aboutMe.js
+++ src/components/aboutMe.js
@@ -1,24 +1,25 @@
 import * as React from 'react'
 import { StaticImage } from 'gatsby-plugin-image'
-import { container } from "./aboutMe.module.scss"
+import {  container,
+          text,
+          image,
+          skillsBox, } from "./aboutMe.module.scss"
+import Programming from "../assets/images/programming.svg"
 
 const AboutMe = () => {
 
   return (
     <section class={container} id="home">
-      <article>
-        <h2>Hello, my name is Joshua Walsh</h2>
+      <h2>Hello, My Name is Joshua Walsh</h2>
+      <article class={text}>
         <p>
-          I'm an experienced software developer specializing in full-stack development. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam ac elit lacinia ex laoreet aliquet. Sed facilisis viverra imperdiet. Curabitur id varius libero, id venenatis libero. Maecenas vel risus porttitor urna aliquet aliquam nec ut mi. Duis in nisl sit amet risus aliquam convallis eu nec ante. Suspendisse id lacus nibh. Nunc porta sodales consectetur. Duis sagittis varius nibh, in vehicula est posuere ac.
+          I'm an experienced software developer specializing in full-stack development. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam ac elit lacinia ex laoreet aliquet. Sed facilisis viverra imperdiet. Curabitur id varius libero, id venenatis libero. 
         </p>
       </article>
-      <div>
-        <StaticImage 
-          alt=""
-          src="../assets/images/programming.svg"
-        />
+      <div class={image}>
+        <Programming />
       </div>
-      <div>
+      <div class={skillsBox}>
         <StaticImage alt="HTML5" src="../assets/images/information.svg" />
         <StaticImage alt="CSS3" src="../assets/images/information.svg" />
         <StaticImage alt="JavaScript" src="../assets/images/information.svg" />
