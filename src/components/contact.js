c083fb5f4cf39e45975080fe8b4303804c3b66c6
JoshuaW2020 <joshua.walsh1998@gmail.com>
2023-09-22 18:16:58 -0700

    Fix Attempt 2 - Project Link Scroll
    Trying a different class name, hoping css will compile differently.

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,12 +1,12 @@
 import * as React from "react"
 import ContactForm from "./contactForm"
-import {  container,
+import {  contactContainer,
           form, } from "./contact.module.scss"
 
 const Contact = () => {
 
   return (
-    <section className={container} id="contact">
+    <section className={contactContainer} id="contact">
       <h3>Like what you see and want to connect? Fill out the form below or checkout my linkedin at the top right!</h3>
       <div className={form}>
         <ContactForm />
